ee458d4dee0c3ac7a6b9988a490896dee805acbf
mihailgaberov <mihail.gaberov@gmail.com>
2021-09-07 09:17:10 +0200

    :white_check_mark: Added tests for Spread component.

---
+++ src/components/Spread/Spread.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import Spread from './index';
+
+test('renders spread component correctly', () => {
+  render(<Spread bids={[[1000, 1], [2000, 2]]} asks={[[999, 1], [1999, 2]]} />);
+  const amountElement = screen.getByText(/Spread: 1,001/i);
+  expect(amountElement).toBeInTheDocument();
+  const percentageElement = screen.getByText(/(50.05%)/i);
+  expect(percentageElement).toBeInTheDocument();
+});
